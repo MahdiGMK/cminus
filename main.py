6a8b81fc08a6dff37e043fb72f25b2f2089c18e5
MahdiGMK <mbahramiand@gmail.com>
2025-11-30 14:20:11 +0330

    some more

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from io import TextIOWrapper
+from io import BufferedReader, TextIOWrapper
+from warnings import simplefilter
 
 
 def isWhiteSpace(chr: bytes) -> bool:
@@ -8,13 +9,25 @@ def isWhiteSpace(chr: bytes) -> bool:
         chr == b'\v' or  chr == b'\f'
     )
 
+BUFFERSIZE = 2048
 class Reader:
-    def __init__(self, source_file: TextIOWrapper) -> None:
+    def __init__(self, source_file: BufferedReader) -> None:
         self.source_file = source_file
-        self.buffer = bytes(0 for i in range(2048))
+        self.buffer = bytearray(0 for i in range(BUFFERSIZE))
         self.start_ptr = 0
         self.end_ptr = 0
 
+    def moveToFirst(self) -> None:
+        for i in range(self.end_ptr - self.start_ptr):
+            self.buffer[i] = self.buffer[i + self.start_ptr]
+        self.end_ptr -= self.start_ptr
+        self.start_ptr = 0
+    def readMore(self) -> None:
+        if self.end_ptr == BUFFERSIZE:
+            self.moveToFirst()
+        next_chunk = self.source_file.read(BUFFERSIZE - self.end_ptr)
+        self.buffer[self.end_ptr:self.end_ptr + len(next_chunk)] = next_chunk
+        self.end_ptr += len(next_chunk)
     def dropWhiteSpaces(self) -> None:
         while (self.start_ptr < self.end_ptr):
             pass
@@ -28,5 +41,5 @@ class Reader:
         return res
 
 if __name__ == "__main__":
-    rdr = Reader(open("test1.c"))
+    rdr = Reader(open("test1.c", "rb"))
     print(f"hello world {rdr.buffer.__len__()}")
