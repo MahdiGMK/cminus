8d9e32099cb23ce15e60e820a38e04568c1b4ec7
radalj <radinjarireh@gmail.com>
2025-12-04 11:23:22 +0330

    tokenizing but no error handling for now

--- main.py
+++ main.py
@@ -1,45 +1,127 @@
 from io import BufferedReader, TextIOWrapper
 from warnings import simplefilter
 
+def is_WhiteSpace(ch: str) -> bool:
+    return len(ch) == 1 and ch in " \t\n\r\v\f"
 
-def isWhiteSpace(chr: bytes) -> bool:
-    return len(chr) == 1 and (
-        chr == b' ' or chr == b'\t' or
-        chr == b'\n' or chr == b'\r' or
-        chr == b'\v' or  chr == b'\f'
-    )
 
-BUFFERSIZE = 2048
-class Reader:
-    def __init__(self, source_file: BufferedReader) -> None:
-        self.source_file = source_file
-        self.buffer = bytearray(0 for i in range(BUFFERSIZE))
-        self.start_ptr = 0
-        self.end_ptr = 0
+def is_Symbol(ch: str) -> bool:
+    return (len(ch) == 1 and ch in ";:,[](){}+-*/=<>") or ch == "=="
 
-    def moveToFirst(self) -> None:
-        for i in range(self.end_ptr - self.start_ptr):
-            self.buffer[i] = self.buffer[i + self.start_ptr]
-        self.end_ptr -= self.start_ptr
-        self.start_ptr = 0
-    def readMore(self) -> None:
-        if self.end_ptr == BUFFERSIZE:
-            self.moveToFirst()
-        next_chunk = self.source_file.read(BUFFERSIZE - self.end_ptr)
-        self.buffer[self.end_ptr:self.end_ptr + len(next_chunk)] = next_chunk
-        self.end_ptr += len(next_chunk)
-    def dropWhiteSpaces(self) -> None:
-        while (self.start_ptr < self.end_ptr):
-            pass
-    def peekToken(self) -> bytes:
-        pass
-    def dropToken(self) -> None:
-        pass
-    def takeToken(self) -> str:
-        res = self.peekToken()
-        self.dropToken()
-        return res
+def is_Keyword(s: str) -> bool:
+    keywords = ["if", "else", "void", "int", "for", "break", "return"]
+    return s in keywords
 
 if __name__ == "__main__":
-    rdr = Reader(open("test1.c", "rb"))
-    print(f"hello world {rdr.buffer.__len__()}")
+    # Open file
+    tokens = []
+    with open("input.txt", "r", encoding="utf-8", errors="ignore") as f:
+        lineno = 0
+        multiple_line_comment_open = False
+        for line in f:
+            line_tokens = []
+            lineno += 1
+            current_token_start = 0
+            line_length = len(line)
+            current_line_comment_open = False
+            current_token_possible_ID = False
+            current_token_possible_NUM = False
+            idx = -1
+            while idx + 1 < line_length:
+                idx = idx + 1
+
+                if idx + 1 < line_length:
+                    look_ahead = line[idx + 1]
+                else:
+                    look_ahead = '\n'
+
+                #comment handling
+                if multiple_line_comment_open:
+                    if (line[idx] != '*' or line[idx + 1] != '/'):
+                        continue
+                    multiple_line_comment_open = False
+                    current_token_start = idx + 2
+                    idx += 1
+                    continue
+
+                if idx == current_token_start:
+                    if line[idx] == '/' and look_ahead == '/':
+                        current_line_comment_open = True
+                        break
+                    if line[idx] == '/' and look_ahead == '*':
+                        multiple_line_comment_open = True
+                        current_token_start = idx + 2
+                        idx += 1
+                        continue
+
+
+                    if is_WhiteSpace(line[idx]):
+                        current_token_start += 1
+                        continue
+
+                    if idx + 1 < line_length and is_Symbol(line[idx:idx + 2]):
+                        line_tokens.append({line[idx:idx + 2]: "SYMBOL"})
+                        current_token_start = idx + 2
+                        idx += 1
+                        continue
+                    if is_Symbol(line[idx]):
+                        line_tokens.append({line[idx]: "SYMBOL"})
+                        current_token_start = idx + 1
+                        continue
+
+                    if line[idx].isalpha() or line[idx] == '_':
+                        current_token_possible_ID = True
+                        continue
+
+                    if line[idx].isdigit():
+                        current_token_possible_NUM = True
+                        continue
+                else:
+                    if current_token_possible_ID:
+                        if line[idx].isalnum() or line[idx] == '_':
+                            continue
+                        else:
+                            token = line[current_token_start:idx]
+                            if is_Keyword(token):
+                                line_tokens.append({token: "KEYWORD"})
+                            else:
+                                line_tokens.append({token: "ID"})
+                            current_token_start = idx
+                            current_token_possible_ID = False
+                            idx -= 1
+                            continue
+
+                    if current_token_possible_NUM:
+                        if line[idx].isdigit():
+                            continue
+                        else:
+                            token = line[current_token_start:idx]
+                            line_tokens.append({token: "NUM"})
+                            current_token_start = idx
+                            current_token_possible_NUM = False
+                            idx -= 1
+                            continue
+
+            if current_token_possible_ID:
+                token = line[current_token_start:line_length]
+                if is_Keyword(token):
+                    line_tokens.append({token: "KEYWORD"})
+                else:
+                    line_tokens.append({token: "ID"})
+
+            elif current_token_possible_NUM:
+                token = line[current_token_start:line_length]
+                line_tokens.append({token: "NUM"})            
+            tokens.append((lineno, line_tokens))
+
+with open("tokens.txt", "w", encoding="utf-8", errors="ignore") as f:
+    for lineno, line_tokens in tokens:
+        if len(line_tokens) == 0:
+            continue
+        f.write(f"{lineno}.\t")
+        for token in line_tokens:
+            for k, v in token.items():
+                f.write(f"({v}, {k}) ")
+        f.write("\n")
+
+        
