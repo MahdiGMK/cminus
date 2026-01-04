dca61fbad6959e2b6a5c0b3a2a45b31f3e6de812
radalj <radinjarireh@gmail.com>
2026-01-04 13:19:17 +0330

    tester improved

--- tester.sh
+++ tester.sh
@@ -1,4 +1,20 @@
-cp phase2-tests/T$1/input.txt input.txt
-python parser.py
-delta syntax_error.txt phase2-tests/T$1/syntax_errors.txt
-delta parse_tree.txt phase2-tests/T$1/parse_tree.txt
+#!/bin/bash
+if [ $# -eq 1 ]; then
+  j="$1"
+  cp "phase2-tests/T$j/input.txt" input.txt
+  python parser.py
+  diff -bBq syntax_error.txt "phase2-tests/T$j/syntax_errors.txt"
+  diff -bBq parse_tree.txt "phase2-tests/T$j/parse_tree.txt"
+else
+  for ((i=1; i<=10; i++)); do
+    if [ "$i" -lt 10 ]; then
+      j="0$i"
+    else
+      j="$i"
+    fi
+    cp "phase2-tests/T$j/input.txt" input.txt
+    python parser.py
+    diff -bBq syntax_error.txt "phase2-tests/T$j/syntax_errors.txt"
+    diff -bBq parse_tree.txt "phase2-tests/T$j/parse_tree.txt"
+  done
+fi
